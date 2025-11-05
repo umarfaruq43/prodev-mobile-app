1d4e955e0cb3c92f9d2f1316d4de127ae4a1e5c5
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-11-05 15:19:09 +0100

    file duplicate

---
+++ prodev-mobile-app-0x05/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { styles } from "@/styles";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return <Stack screenOptions={{
+    headerShown: false,
+  }}>
+    <Stack.Screen name="index" />
+    <Stack.Screen name="join" />
+    <Stack.Screen name="signin" />
+  </Stack>;
+}
