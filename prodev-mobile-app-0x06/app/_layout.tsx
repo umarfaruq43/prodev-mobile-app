1067eb0c23110a50dbc0e9596761c4470f03b955
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-11-06 12:32:54 +0100

    fix: layout fix

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -3,6 +3,7 @@ import "../styles/global.css"
 
 export default function RootLayout() {
   return <Stack screenOptions={{
+    headerShown: false,
   }}>
     <Stack.Screen name="index" />
     <Stack.Screen name="join" />
